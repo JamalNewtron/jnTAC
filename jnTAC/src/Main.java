b06479d6be884a84d82dfdea25e800f2e174cd42
JamalNewtron <j.panah@outlook.com>
2020-07-19 10:31:07 +0200

    Added comment for VCS testing.

--- jnTAC/src/Main.java
+++ jnTAC/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // just for testing
+
         // four mumbles for first player
         Mumble firstPlayerFirstMumble = new Mumble();
         Mumble firstPlayerSecondMumble = new Mumble();
